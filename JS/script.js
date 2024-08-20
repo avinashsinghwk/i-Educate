6bebfb8ea0b3ea7d5db851287fcece12688ae622
Avinash Kumar Singh <abhinashsinghwk2@gmail.com>
2024-08-20 05:50:14 +0530

    Add all folder and files

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+document.getElementById("burger").onclick = () => {
+    document.getElementById("navbar").classList.toggle("h-nav-resp");
+
+    document.getElementById("nav-list").classList.toggle("v-class-resp");
+
+
+    document.getElementById("rightnav").classList.toggle("v-class-resp");
+
+
+}
+
+
